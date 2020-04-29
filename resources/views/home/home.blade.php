23cc255c3826f700baa0715ec3b06d29b24ecdcb
Walburn, Micheal <26194597+dgtldrifter@users.noreply.github.com>
2020-04-29 17:43:04 -0500

    changed styling of upload card

--- resources/views/home/home.blade.php
+++ resources/views/home/home.blade.php
@@ -6,15 +6,26 @@
 <div class="container">
     <div class="row justify-content-center">
         <div class="col-md-8">
-            @if(session('message'))
-                <p>{{ session('message') }}</p>
-            @endif
+            <div class="card">
+                <div class="card-header">
+                    Import Data from CSV
+                </div>
+                <div class="card-body">
+                    <form method="post" action="/uploadFile" enctype="multipart/form-data">
+                        <div class="form-group">
+                            {{ csrf_field() }}
+                            <input type="file" class="form-control-file" name="file"/>
+                        </div>
 
-            <form method="post" action="/uploadFile" enctype="multipart/form-data">
-                {{ csrf_field() }}
-                <input type="file" name="file"/>
-                <input type="submit" name="submit" value="Import"/>
-            </form>
+                        <input type="submit" class="btn btn-primary" name="submit" value="Import"/>
+                    </form>
+                </div>
+                @if(session('message'))
+                    <div class="card-footer text-muted">
+                            {{ session('message') }}
+                    </div>
+                @endif
+            </div>
         </div>
     </div>
 </div>
