8070269652e6117df7c222d22a24a6920b17f970
Walburn, Micheal <26194597+dgtldrifter@users.noreply.github.com>
2020-05-12 11:24:56 -0500

    export function

--- app/Client.php
+++ app/Client.php
@@ -31,9 +31,17 @@ class Client extends Model
     }
 
     public static function getClientsForExport() {
-        return DB::select(DB::raw("select c.first_name, c.last_name, COALESCE(u.email, 'N/A') as 'User Account'
-                                from clients as c
-                                left join users as u on c.email = u.email
-                                where c.user_id = ".auth()->user()->id));
+        DB::raw("drop view clients_users");
+
+        DB::raw("create view clients_users as
+            select users.id as UserID, clients.first_name, clients.last_name, COALESCE(users.email , 'N/A') as UserAccount, clients.email as ClientEmail
+            from clients
+            left join users on users.email = clients.email
+            where clients.user_id = 1");
+
+        return DB::select(DB::raw("select clients_users.first_name, clients_users.last_name, clients_users.UserAccount, count(clients.id) as numberOfClients
+            from clients
+            right join clients_users on clients_users.UserID = clients.user_id
+            group by ClientEmail"));
     }
 }
